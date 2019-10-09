ec7818e4509b6f6e5e62b20445ac0c12da806e5a
Jay Ferguson <ferguson.368@gmail.com>
2019-10-09 13:29:32 -0700

    Changed fibonacci func to recursive.

--- students/jay_ferguson/lesson02/series.py
+++ students/jay_ferguson/lesson02/series.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-
 def fibonacci(n):
     """Return the nth value of a Fibonacci sequence."""
-    fib_list = [0, 1]
-    fib_a = 0
-    fib_b = 1
-    if n == 0 or n == 1:
-        return fib_list[n]
-    else:
-        for i in range(n + 1):
-            fib = fib_a + fib_b
-            fib_a = fib_b
-            fib_b = fib
-        return fib
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1 )
+
+
+if __name__ == '__main__':
+    n = int(input("n : "))
+    print(fibonacci(n))
+
 
