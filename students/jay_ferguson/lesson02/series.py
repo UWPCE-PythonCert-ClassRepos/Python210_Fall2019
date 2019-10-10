92155e3b4abf829635b90b042be02431dd6aeb97
Jay Ferguson <ferguson.368@gmail.com>
2019-10-09 17:07:52 -0700

    Added assertion tests.

--- students/jay_ferguson/lesson02/series.py
+++ students/jay_ferguson/lesson02/series.py
@@ -2,39 +2,62 @@
 
 def fibonacci(n):
     """Return the nth value of a Fibonacci sequence."""
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    return fibonacci(n - 2) + fibonacci(n - 1 )
+
+    return sum_series(n)
 
 def lucas(n):
     """Return the nth value of a Lucas sequence."""
-    if n == 0:
-        return 2
-    elif n == 1:
-        return 1
-    return lucas(n - 2) + lucas(n - 1)
+
+    return sum_series(n, n0=2, n1=1)
 
 
-def sum_series(n, pos_1=0, pos_2=1):
-    """Return the nth value of a Fibonacci sequence if pos_1=0 and pos_2=1.
+def sum_series(n, n0=0, n1=1):
+    """Return the nth value of a Fibonacci sequence if n0=0 and n1=1.
     Else, return the nth value of a Lucas sequence."""
 
-    if pos_1 == 0 and pos_2 == 1:
+    if n0 == 0 and n1 == 1:
         if n == 0:
             return 0
         elif n == 1:
             return 1
-    elif pos_1 == 2 and pos_2 == 1:
+    else:
         if n == 0:
-            return 2
+            return n0
         elif n == 1:
-            return 1
-    return sum_series(n - 2, pos_1, pos_2) + sum_series(n - 1, pos_1, pos_2)
+            return n1
+    return sum_series(n - 2, n0, n1) + sum_series(n - 1, n0, n1)
 
 
-if __name__ == '__main__':
-    print(sum_series(3, 2, 1))
+if __name__ == "__main__":
+    # run some tests
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
 
+    assert lucas(0) == 2
+    assert lucas(1) == 1
+
+    assert lucas(4) == 7
+
+    # test that sum_series matches fibonacci
+    assert sum_series(5) == fibonacci(5)
+    assert sum_series(7, 0, 1) == fibonacci(7)
+
+    # test if sum_series matched lucas
+    assert sum_series(5, 2, 1) == lucas(5)
+
+    # test if sum_series works for arbitrary initial values
+    assert sum_series(0, 3, 2) == 3
+    assert sum_series(1, 3, 2) == 2
+    assert sum_series(2, 3, 2) == 5
+    assert sum_series(3, 3, 2) == 7
+    assert sum_series(4, 3, 2) == 12
+    assert sum_series(5, 3, 2) == 19
+
+    print("tests passed")
 
