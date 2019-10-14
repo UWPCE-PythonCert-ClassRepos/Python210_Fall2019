e0986edd51a43d3d31dc224e0d3460171b692a0b
Jammy Chong <jammy087@gmail.com>
2019-10-13 21:21:12 -0700

    completed lesson 01 tasks

---
+++ students/jammy_chong/lesson01/Warmup1_parrot_trouble.py
@@ -0,0 +1,24 @@
+"""
+We have a loud talking parrot.
+The "hour" parameter is the current hour time in the range 0..23.
+We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+
+Return True if we are in trouble.
+"""
+
+def parrot_trouble(talking, hour):
+  if talking and (hour < 7 or hour > 20):
+    return True
+  else:
+    return False
+
+print(parrot_trouble(True, 6))
+print(parrot_trouble(True, 7))
+print(parrot_trouble(False, 6))
+print(parrot_trouble(True, 21))
+print(parrot_trouble(False, 21))
+print(parrot_trouble(False, 20))
+print(parrot_trouble(True, 23))
+print(parrot_trouble(False, 23))
+print(parrot_trouble(True, 20))
+print(parrot_trouble(False, 12))
---
+++ students/jammy_chong/lesson01/Warmup2_string_bits.py
@@ -0,0 +1,11 @@
+"""
+Given a string, return a new string made of every other char
+starting with the first, so "Hello" yields "Hlo".
+"""
+
+def string_bits(str):
+  new_str = ""
+  for n in range(0, len(str), 2):
+    new_str = new_str + str[n]
+
+  return new_str
