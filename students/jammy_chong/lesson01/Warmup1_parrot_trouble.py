b9702eed633e049bf47eba6642a43cf84caacc7a
Jammy Chong <jammy087@gmail.com>
2019-10-14 23:13:43 -0700

    Completed Lesson 02 tasks.

--- students/jammy_chong/lesson01/Warmup1_parrot_trouble.py
+++ students/jammy_chong/lesson01/Warmup1_parrot_trouble.py
@@ -7,10 +7,7 @@ Return True if we are in trouble.
 """
 
 def parrot_trouble(talking, hour):
-    if talking and (hour < 7 or hour > 20):
-        return True
-    else:
-        return False
+    return talking and (hour < 7 or hour > 20)
 
 print(parrot_trouble(True, 6))
 print(parrot_trouble(True, 7))
