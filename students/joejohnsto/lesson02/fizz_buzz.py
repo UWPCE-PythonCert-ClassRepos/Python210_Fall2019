1602e2642e8720775a9ea222b25907dc7926ee44
joejohnsto <joejohnsto@gmail.com>
2019-10-15 19:34:55 -0700

    Create fizz_buzz.py
    lesson02 exerise fizzbuzz complete

---
+++ students/joejohnsto/lesson02/fizz_buzz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 19:28:36 2019
+
+@author: joejo
+"""
+
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+     print('FizzBuzz')
+    elif i%5==0:
+        print('Buzz')
+    elif i%3==0:
+        print('Fizz')
+    else:
+        print(i)
