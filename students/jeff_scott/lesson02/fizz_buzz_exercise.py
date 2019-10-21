7130bc694d78ef8610cc9617b1e2d28cdf7f3bfb
Jeff Scott <jscott@tableau.com>
2019-10-21 14:30:05 -0700

    Added comment for range function and logic operator

--- students/jeff_scott/lesson02/fizz_buzz_exercise.py
+++ students/jeff_scott/lesson02/fizz_buzz_exercise.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-# Print an entire range of 1-100
+"""Print an entire range of 1-100"""
+
+
 def fizzbuzz_range ():
     for number in range(1, 101):
         print(number)
 
-# Do math
+
+"""Do math, use built in range function, and logic operator to return true for numbers divisible by 3 and 5"""
+
+
 def fizzbuzz ():
     for number in range(1, 101):
         if not number % 3 and not number % 5:
