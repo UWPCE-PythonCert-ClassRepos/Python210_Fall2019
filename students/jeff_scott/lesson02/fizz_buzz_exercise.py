3588ebdfd6c41c171b125c96b081d8c478706d04
Jeff Scott <jscott@tableau.com>
2019-10-21 14:18:35 -0700

    Added function

--- students/jeff_scott/lesson02/fizz_buzz_exercise.py
+++ students/jeff_scott/lesson02/fizz_buzz_exercise.py
@@ -1,6 +1,21 @@
-def fizzbuzz ():
-    for number in range(1, 100):
+#!/usr/bin/env python3
+
+# Print an entire range of 1-100
+def fizzbuzz_range ():
+    for number in range(1, 101):
         print(number)
+
+# Do math
+def fizzbuzz ():
+    for number in range(1, 101):
+        if not number % 3 and not number % 5:
+            print("FizzBuzz")
+        elif not number % 3:
+            print('Fizz')
+        elif not number % 5:
+            print('Buzz')
+        else:
+            print(number)
 
 
 if __name__ == '__main__':
