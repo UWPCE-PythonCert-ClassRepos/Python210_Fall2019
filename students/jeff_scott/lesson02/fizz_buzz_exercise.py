bf4427400a081305136958f88e171d55b06eee05
Jeff Scott <jscott@tableau.com>
2019-10-22 15:01:38 -0700

    Added more docstring

--- students/jeff_scott/lesson02/fizz_buzz_exercise.py
+++ students/jeff_scott/lesson02/fizz_buzz_exercise.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
-"""Print an entire range of 1-100"""
-
 
 def fizzbuzz_range ():
     for number in range(1, 101):
         print(number)
-
-
-"""Do math, use built in range function, and logic operator to return true for numbers divisible by 3 and 5"""
-
+        """Print entire range 1-100 inclusive"""
 
 def fizzbuzz ():
     for number in range(1, 101):
         if not number % 3 and not number % 5:
+            """If divisible by 3 and 5 is true print Fizzbuzz"""
             print("FizzBuzz")
         elif not number % 3:
+            """If divisible by 3 is true print Fizz"""
             print('Fizz')
         elif not number % 5:
+            """If divisible by 5 is true print Buzz"""
             print('Buzz')
         else:
+            """Just print the number if none of the above are true"""
             print(number)
 
 
--- students/jeff_scott/lesson02/grid_printer_exercise.py
+++ students/jeff_scott/lesson02/grid_printer_exercise.py
@@ -2,7 +2,6 @@
 
 # Part 1 #
 
-
 # Part 2 #
 
 # Part 3 #
