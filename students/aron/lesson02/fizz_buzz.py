f1c1a89687d3bbb1c5256d174bdad03fdcd8d135
Aron Watson <awatson@awatson-mac1.tsi.lan>
2019-10-22 19:53:02 -0700

    Lesson 02 Exercise: Fizz Buzz

--- students/aron/lesson02/fizz_buzz.py
+++ students/aron/lesson02/fizz_buzz.py
@@ -1,10 +1,17 @@
-for i in range(1,101):
 
-    if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
-    else:
-        print(i)
+
+def fizzbuzz():
+    for i in range(1,101):
+
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
+if __name__ == '__main__':
+    fizzbuzz()
