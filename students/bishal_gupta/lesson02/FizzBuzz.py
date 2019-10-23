6ffff284b342a2a938ffac8f7da06e5dd4972f84
Bishal K Gupta <bishalg@gmail.com>
2019-10-22 19:30:37 -0700

    Add modular capability

--- students/bishal_gupta/lesson02/FizzBuzz.py
+++ students/bishal_gupta/lesson02/FizzBuzz.py
@@ -4,16 +4,20 @@
 #For numbers that are multiples of both 3 and 5, the program prints "FizzBuzz".
 #For numbers that are neither multiples of 3 or 5, just program prints the number as is
 
-n = 0
-n_max = 100
+def FizzBuzz():
+    n = 0
+    n_max = 100
 
-while n != n_max:
-    n += 1
-    if (n % 3 == 0 and n % 5 != 0):
-        print('Fizz')
-    elif (n % 3 != 0 and n % 5 == 0):
-        print('Buzz')
-    elif (n % 3 == 0 and n % 5 == 0):
-        print('FizzBuzz')
-    else:
-        print(n)
+    while n != n_max:
+        n += 1
+        if (n % 3 == 0 and n % 5 != 0):
+            print('Fizz')
+        elif (n % 3 != 0 and n % 5 == 0):
+            print('Buzz')
+        elif (n % 3 == 0 and n % 5 == 0):
+            print('FizzBuzz')
+        else:
+            print(n)
+
+if __name__=='__main__':
+    FizzBuzz()
