0e72a8a4eb41ee05d7fe8a9e5fc1f6c2c9465812
Philip Korte <kortephilip@gmail.com>
2019-10-22 19:32:56 -0700

    I made a change to fizz_buzz to make sure it more accurately completes the requirments of the assigment

--- students/philip_korte/lesson02/fizz_buzz.py
+++ students/philip_korte/lesson02/fizz_buzz.py
@@ -8,6 +8,8 @@ fizz_buzz.py
 for i in range(101):
     if i % 3 == 0 and i % 5 == 0:
         print(i, 'FizzBuzz')
+    elif i % 3 == 0:
+        print(i, 'Fizz')
     elif i % 5 == 0:
         print(i, 'Buzz')
     else:
