cb79bc4c78c129d58061d372cad90f18d09f2cf5
buckcorter <bc.lastname@gmail.com>
2019-10-26 17:21:05 -0700

    Added assert tests to each function

--- students/ben_carter/lesson03/slicinglab.py
+++ students/ben_carter/lesson03/slicinglab.py
@@ -27,29 +27,34 @@ def exchange_first_last(seq):
     copy_list.insert(0, copy_list.pop(-2))
     return copy_list
 
-#assert exchange_first_last(listofpets) == ["rock","dog","cat","bird","snake","spider","ferret","monkey","hippo","lizard"]
-
+listofpets = ["lizard","dog","cat","bird","snake","spider","ferret","monkey","hippo","rock"]
+assert exchange_first_last(listofpets) == ["rock","dog","cat","bird","snake","spider","ferret","monkey","hippo","lizard"]
 
 def remove_every_other(seq):
     """This function removes every other item in a sequence"""
     every_other = seq[0::2]
     return every_other
-    
-#assert remove_every_other(listofpets) == ['lizard', 'cat', 'snake', 'ferret', 'hippo']
+
+listofpets = ["lizard","dog","cat","bird","snake","spider","ferret","monkey","hippo","rock"]    
+assert remove_every_other(listofpets) == ['lizard', 'cat', 'snake', 'ferret', 'hippo']
 
 def remove_first_four_last_four_every_other(seq):
     """This function removes the first four items in a sequence, the
     last four items in a sequence, and than every other item of what remains"""
     copy_list = seq
     return copy_list[4:-4:2]
-    
+
+listofpets = ["lizard","dog","cat","bird","snake","spider","ferret","monkey","hippo","rock"]    
+assert remove_first_four_last_four_every_other(listofpets) == ["snake"]
     
 def reverse_order(seq):
     """This function flips the sequence so all items are backwards"""
     copy_list = seq
     return copy_list[::-1]
-    
-    
+
+listofpets = ["lizard","dog","cat","bird","snake","spider","ferret","monkey","hippo","rock"]    
+assert reverse_order(listofpets) == ['rock','hippo','monkey','ferret','spider','snake','bird','cat','dog','lizard']    
+
 def chop_reorder(seq):
     """This function chops the sequence into thirds and reorders them"""
     seq_length = len(seq)
@@ -59,7 +64,7 @@ def chop_reorder(seq):
     original_third = seq[-x::]
     return original_third + original_first + original_second
     
-    
+assert chop_reorder(tupleofnum) == (15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
     
     
     
