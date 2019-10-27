0048acda0c27ed1c7abb97ced99deff1fd7a8ab1
buckcorter <bc.lastname@gmail.com>
2019-10-26 17:10:47 -0700

    finished the rest of the functions for slicing lab

--- students/ben_carter/lesson03/slicinglab.py
+++ students/ben_carter/lesson03/slicinglab.py
@@ -18,7 +18,7 @@ write a function that chops the sequence into thirds and puts the last
 third first, and the first in the middle and the middle at the end. 
 """
 listofpets = ["lizard","dog","cat","bird","snake","spider","ferret","monkey","hippo","rock"]
-tupleofnum = 31, 4, 5, 19, 29, 12, 54, 17, 83, 56, 92, 154
+tupleofnum = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
 
 def exchange_first_last(seq):
     """This function swaps the first and last items in a sequence"""
@@ -27,18 +27,69 @@ def exchange_first_last(seq):
     copy_list.insert(0, copy_list.pop(-2))
     return copy_list
 
-assert exchange_first_last(listofpets) == ["rock","dog","cat","bird","snake","spider","ferret","monkey","hippo","lizard"]
+#assert exchange_first_last(listofpets) == ["rock","dog","cat","bird","snake","spider","ferret","monkey","hippo","lizard"]
 
 
 def remove_every_other(seq):
     """This function removes every other item in a sequence"""
+    every_other = seq[0::2]
+    return every_other
     
+#assert remove_every_other(listofpets) == ['lizard', 'cat', 'snake', 'ferret', 'hippo']
+
 def remove_first_four_last_four_every_other(seq):
     """This function removes the first four items in a sequence, the
     last four items in a sequence, and than every other item of what remains"""
+    copy_list = seq
+    return copy_list[4:-4:2]
+    
     
 def reverse_order(seq):
     """This function flips the sequence so all items are backwards"""
+    copy_list = seq
+    return copy_list[::-1]
+    
     
 def chop_reorder(seq):
     """This function chops the sequence into thirds and reorders them"""
+    seq_length = len(seq)
+    x = int((seq_length) / 3)
+    original_first = seq[:x:]
+    original_second = seq[x:-x:]
+    original_third = seq[-x::]
+    return original_third + original_first + original_second
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
