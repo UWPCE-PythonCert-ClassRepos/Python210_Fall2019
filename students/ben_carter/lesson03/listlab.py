29c15884e3f2fde47100f9c2fc0d805491dd1bbe
buckcorter <bc.lastname@gmail.com>
2019-10-27 20:12:55 -0700

    Listlab.py complete up to series 3.

--- students/ben_carter/lesson03/listlab.py
+++ students/ben_carter/lesson03/listlab.py
@@ -6,6 +6,8 @@ Created on Thu Oct 24 17:48:39 2019
 @author: bclas
 """
 
+#series 1
+
 fruits = ["apples","pears","oranges","peaches"]
 
 print(fruits)
@@ -19,7 +21,7 @@ print(fruits[fruit_index - 1])
 print(["pineapple"] + fruits)
 
 fruits.insert(0,"guava")
-print (fruits)
+print(fruits)
 
 p_fruits = []
 for word in fruits:
@@ -27,3 +29,25 @@ for word in fruits:
        p_fruits.append(word)
 
 print(p_fruits)
+
+#series 2
+print(fruits)
+del fruits[-1::]
+print(fruits)
+
+delete_fruit = input("please type a fruit to delete from the list:")
+fruits.remove(delete_fruit) 
+print(fruits)
+
+#series 3 
+
+for x in fruits:
+    answer = input("Do you like {}?\n".format(x))
+    while answer != 'No' and answer != 'Yes':
+        answer = input("Please respond with 'Yes' or 'No'.\n")
+    if answer == 'No':
+        fruits.remove(x)
+    elif answer == 'Yes':
+        continue
+print(fruits)
+
