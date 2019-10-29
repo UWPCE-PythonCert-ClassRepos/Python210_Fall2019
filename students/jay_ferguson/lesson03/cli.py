b7ce6631e57f88c4486f2fc648c74509bbfd9317
Jay Ferguson <ferguson.368@gmail.com>
2019-10-29 09:44:03 -0700

    Moved CLI to cli.py. Finished mailroom.py.

---
+++ students/jay_ferguson/lesson03/cli.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+
+class CLI():
+    """
+    Class to present a CLI to the user.
+    """
+    def __init__(self, cli_functions=None):
+        """
+        Initialize the CLI class.
+        :param cli_functions: Optionally pass in a list of functions at instantiation.
+        """
+        if cli_functions is None:
+            cli_functions = []
+        cli_functions.append(self.quit)  # Add quit() to our probably empty list of functions
+        self.cli_functions = cli_functions
+        self.cli_message = ("Menu Options\n")
+        self.cli_options_template = "{}) {}\n"
+
+        self.generate_cli()
+
+        #Should we continue to display the prompt? Set to False to stop.
+        self.continue_session = True
+
+
+    def generate_cli(self):
+        """
+        Generate the CLI. Updates class attributes cli_options, cli_prompt.
+        :param cli_functions: List of functions that will be the CLI options
+        :return: None
+        """
+        options = [i.__name__ for i in self.cli_functions]
+        options = [i.replace("_", " ").title() for i in options]
+
+        # Humans like to start from 1. Add 1 to the index so we start at 1 rather than 0.
+        cli_options = [self.cli_options_template.format(options.index(i) + 1, i) for i in options]
+
+        self.cli_options = cli_options
+
+        cli_prompt = self.cli_message
+        for option in cli_options:
+            cli_prompt += option
+
+        self.cli_prompt = cli_prompt
+
+
+    def display_prompt(self):
+        """
+        Display the generated CLI prompt
+        :return: None
+        """
+        if self.continue_session:
+
+            print(self.cli_prompt)
+            return self.continue_session
+
+        else:
+            return self.continue_session
+
+    def add_option(self, function):
+        """
+        Wrapper to add a function to CLI. Re-generates the prompt and updates cli_options param.
+        Designed to be used as a decorator.
+        :param function: function object
+        :return: None
+        """
+        # Make sure that quit() is always the last function.
+        self.cli_functions.remove(self.quit)
+        self.cli_functions.append(function)
+        self.cli_functions.append(self.quit)
+        self.generate_cli()
+
+    def quit(self):
+        """
+        Leave the program.
+        :return: None
+        """
+
+        self.continue_session = False
+
+    def run_cli(self):
+        """
+        Display the CLI to the user and prompt for input.
+        :return: None
+        """
+
+        def get_function():
+            """
+            Helper function to  return the function to run based on the option selected.
+            :return: Function object.
+            """
+
+            try:
+                user_input = input('Please select option: ')
+            except KeyboardInterrupt:
+                return self.quit
+
+            # Did we get an option number?
+            try:
+                user_input = int(user_input)
+                function_index = user_input
+
+            except ValueError:  # We got a string. Check if it's a valid option.
+                lowercase_options = [i.lower().replace('\n', '')[3:] for i in self.cli_options]
+
+                if user_input.lower() in lowercase_options:
+                    function_index = lowercase_options.index(user_input) + 1  # Because we added 1 to the options index
+
+                else:
+                    print("Invalid selection.\n\n")
+                    return get_function()
+
+            try:
+                selected_function = self.cli_functions[function_index - 1]  # Remove that 1 we added earlier
+                return selected_function
+
+            except IndexError:
+                print("Invalid selection.\n\n")
+                return get_function()
+
+
+        while self.display_prompt():
+
+            function = get_function()
+            function()
--- students/jay_ferguson/lesson03/mailroom.py
+++ students/jay_ferguson/lesson03/mailroom.py
@@ -1,103 +1,137 @@
 #!/usr/bin/env python3
 
-
-class CLI():
-    """
-    Class to present a CLI to the user.
-    """
-    def __init__(self, cli_functions=[]):
-        """
-        Initialize the CLI class.
-        :param cli_functions: Optionally pass in a list of functions at instantiation.
-        """
-        cli_functions.append(self.quit)  # Add quit() to our probably empty list of functions
-        self.cli_functions = cli_functions
-        self.cli_message = ("Menu Options\n")
-        self.cli_options_template = "{}) {}\n"
-
-        self.generate_cli()
-
-        #Should we continue to display the prompt? Set to False to stop.
-        self.continue_session = True
-
-
-    def generate_cli(self):
-        """
-        Generate the CLI. Updates class attributes cli_options, cli_prompt.
-        :param cli_functions: List of functions that will be the CLI options
-        :return: None
-        """
-        options = [i.__name__ for i in self.cli_functions]
-        options = [i.replace("_", " ").title() for i in options]
-
-        # Humans like to start from 1. Add 1 to the index so we start at 1 rather than 0.
-        cli_options = [self.cli_options_template.format(options.index(i) + 1, i) for i in options]
-
-        self.cli_options = cli_options
-
-        cli_prompt = self.cli_message
-        for option in cli_options:
-            cli_prompt += option
-
-        self.cli_prompt = cli_prompt
-
-
-    def display_prompt(self):
-        """
-        Display the generated CLI prompt
-        :return: None
-        """
-        if self.continue_session:
-
-            print(self.cli_prompt)
-            return self.continue_session
-
-        else:
-            return self.continue_session
-
-    def add_option(self, function):
-        """
-        Wrapper to add a function to CLI. Re-generates the prompt and updates cli_options param.
-        Designed to be used as a decorator.
-        :param function: function object
-        :return: None
-        """
-        # Make sure that quit() is always the last function.
-        self.cli_functions.remove(self.quit)
-        self.cli_functions.append(function)
-        self.cli_functions.append(self.quit)
-        self.generate_cli()
-
-    def quit(self):
-        """
-        Leave the program.
-        :return: None
-        """
-
-        self.continue_session = False
-
+from cli import CLI
 
 cli = CLI()
 
-@cli.add_option
-def send_message():
-    pass
+donors = [
+    {
+        'Donor Name': 'William Gates, III',
+        'Total Given': 653784.49,
+        'Num Gifts' : 2
+    },
+    {
+        'Donor Name': 'Mark Zuckerberg',
+        'Total Given': 16396.10,
+        'Num Gifts':  3
+    },
+    {
+        'Donor Name': 'Jeff Bezos',
+        'Total Given':  877.33,
+        'Num Gifts': 1
+    },
+    {
+        'Donor Name': 'Paul Allen',
+        'Total Given': 708.42,
+        'Num Gifts': 3
+    },
+    {
+        'Donor Name': 'Jay Ferguson',
+        'Total Given': 21.12,
+        'Num Gifts': 2
+    }
+]
 
 @cli.add_option
-def add_donor():
-    pass
+def send_thank_you():
+    """
+    If the user types list show them a list of the donor names and re-prompt.
+    If the user types a name not in the list, add that name to the data structure and use it.
+    If the user types a name in the list, use it.
+    This function makes the dubious assertion that first names will be first when entered.
+    :return: None
+    """
+
+    #TODO: Most of the nested structures here should be discrete functions.
+
+    message = """Dear {first_name},
+    
+    Thank you for your generous donation!
+     
+     Best Regards,
+     
+     The UW Python Program"""
+
+    # Generate a list of current donor names to compare against
+    donor_names = [i['Donor Name'] for i in donors]
+
+    prompt = True  # Set to false to stop prompting and exit.
+    while prompt:
+        try:
+            user_input = input('Please enter donor name: ')
+        except KeyboardInterrupt:
+            print('\n')
+            return None
+
+        if user_input == 'list':
+            for name in donor_names: print(name)
+            print('\n')
+
+        else:
+            #Find whether the name is in the list:
+
+            found = False  # Set to True to skip data input
+
+            for name in donor_names:
+                if user_input.lower() in name.lower():
+                    print (message.format(first_name=name.split(' ')[0]))
+                    print('\n')
+                    prompt = False
+                    found = True
+                    return None
+                else:
+                    donor_name = user_input.title()
+
+            if not found: # Start prompting for data and write it to the donors global.
+                total_given = input('Donation Amount: ')
+                #Sanitize our input a bit
+                total_given = round(float(total_given.replace('$', '')), ndigits=2)
+                num_gifts = 1
+
+                new_donor = {
+                    'Donor Name': donor_name,
+                    'Total Given': total_given,
+                    'Num Gifts': num_gifts
+                }
+
+                donors.append(new_donor)
+
+                print (message.format(first_name=donor_name.split(' ')[0]))
+                print('\n')
+                prompt = False
+
 
 @cli.add_option
 def generate_report():
-    pass
+    """
+    Generate a report. And print it.
+    :return: None
+    """
+    sorted_donors = sorted(donors, key=lambda i: i['Total Given'], reverse=True)  # Hooray lambda fuctions!
+    spacing = 20  # Blank space to pad rows with
+    header_keys = list(donors[0].keys())
+    header_keys.append('Average Gift')
 
+    # Calculate average donation and append it to the sorted_donors dicts
+    for i in sorted_donors:
+        average_donation = round(i['Total Given'] / i['Num Gifts'], ndigits=2)
+        i['Average Gift'] = average_donation
+
+    header = '\n' + ('|  {:<20}' * len(header_keys))
+    formatted_header = header.format(*header_keys)
+    row_template = '|  {:<20}|  ${:<20}|  {:<20}|   ${:<20}'
+    print(formatted_header)
+    divider = '-'* len(formatted_header)
+    print(divider)
+
+    for i in sorted_donors:
+        row = []
+        for key in header_keys:
+            row.append(i[key])
+        print(row_template.format(*row))
+
+    print('')
 
 if __name__ == '__main__':
 
-    while cli.display_prompt():
-
-        x = input('Hey there :')
-        print(x)
-
-        if x == '3':
-            cli.quit()
+    cli.run_cli()
