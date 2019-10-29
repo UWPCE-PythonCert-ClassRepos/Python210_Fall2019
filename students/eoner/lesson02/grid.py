6d54650914bcf10accee9b42ca4d72b83476bfd9
Eddie Oner <breakah@gmail.com>
2019-10-28 23:36:07 -0700

    got rid of variables

--- students/eoner/lesson02/grid.py
+++ students/eoner/lesson02/grid.py
@@ -1,9 +1,6 @@
 def print_grid(x, s):
-    a = (((("+" + ("-"*s)))*x) + "+")
-    b = ("\n"+((("|" + (" "*s)))*x) + "|")
-
     for i in range(x):
-        print(a+(b*s))
-    print(a)
+        print((((("+" + ("-"*s)))*x) + "+")+(("\n"+((("|" + (" "*s)))*x) + "|")*s))
+    print(((("+" + ("-"*s)))*x) + "+")
 
 print_grid(3,4) 
