aed9055f1329157ce08009f3aaeb6c3c66df35b9
kclark75 <kclark75@hotmail.com>
2019-10-29 17:20:20 -0700

    kclark75 adding list_lab, strformat_lab ready for teacher reveiw.

--- students/kclark75/assignment03/slicing_lab.py
+++ students/kclark75/assignment03/slicing_lab.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ##############################
-# Assignement: PrintedGrid.py
+# Assignement: slicing_lab.py
 # Dev: Kenneth Clark
 # Date: 10-13-19
 # Class: Python210A Winter
 #############################
+
+"""
+Write some functions that take a sequence as an argument,
+and return a copy of that sequence:
+
+with the first and last items exchanged.
+with every other item removed.
+with the first 4 and the last 4 items removed, and then every other item in
+the remaining sequence.
+with the elements reversed (just with slicing).
+with the last third, then first third, then the middle third in the new order.
+"""
 
 def exchange_first_last(seq):
     a_new_sequence = ''
