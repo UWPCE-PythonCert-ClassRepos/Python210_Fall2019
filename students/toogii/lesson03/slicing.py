aa8310530c4ee2a40cacd6de4875070a032ba4f6
Dashdavaa <dashdava@38f9d369ffca.ant.amazon.com>
2019-10-30 09:38:56 -0700

    Fixing the last_third_first_third_middle_third solution

--- students/toogii/lesson03/slicing.py
+++ students/toogii/lesson03/slicing.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-from copy import copy
 
 
 def first_and_last_item_exchanged(seq):
-    return copy((seq[-1:] + seq[1:-1] + seq[0:1]))
+    return seq[-1:] + seq[1:-1] + seq[0:1]
 
 
 def every_other_item_removed(seq):
-    return copy(seq[::2])
+    return seq[::2]
 
 
 def reversed(seq):
@@ -15,7 +14,7 @@ def reversed(seq):
 
 
 def last_third_first_third_middle_third_order(seq):
-    return copy((seq[-3:] + seq[:3] + seq[int(len(seq)/2 - 1): int(len(seq)/2)+2 ]))
+    return seq[int(len(seq)*2/3+1):] + seq[:int(len(seq)/3+1)] + seq[int(len(seq)/3+1): int(len(seq)*2/3)+1 ]
 
 
 def first_4_last_4_removed(seq):
@@ -35,9 +34,9 @@ if __name__ == '__main__':
     assert reversed(['apple', 'orange', 'banana']) == ['banana', 'orange', 'apple']
     assert reversed(('apple', 'orange', 'banana')) == ('banana', 'orange', 'apple')
 
-    assert last_third_first_third_middle_third_order('1234567891011121314') == '314123910'
-    assert last_third_first_third_middle_third_order(['one','two','three','four','five','six','seven', 'eight','nine','ten','eleven']) == ['nine', 'ten', 'eleven', 'one', 'two', 'three', 'five', 'six', 'seven']
-    assert last_third_first_third_middle_third_order(('one','two','three','four','five','six','seven', 'eight','nine','ten','eleven')) == ('nine', 'ten', 'eleven', 'one', 'two', 'three', 'five', 'six', 'seven')
+    assert last_third_first_third_middle_third_order('abcdefghjklmno') == 'lmnoabcdefghjk'
+    assert last_third_first_third_middle_third_order(['one','two','three','four','five','six','seven', 'eight','nine','ten','eleven']) == ['nine', 'ten', 'eleven', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+    assert last_third_first_third_middle_third_order(('one','two','three','four','five','six','seven', 'eight','nine','ten','eleven')) == ('nine', 'ten', 'eleven', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight')
 
     assert first_4_last_4_removed('appL012345678Uice') == '02468'
     assert first_4_last_4_removed([1,2,3,4,5,1,5,1,5,1,6,7,8,9]) == [5,5,5]
