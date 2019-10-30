cdc5953a8e738af72c621c1e2ed045986a1fc786
Bishal K Gupta <bishalg@gmail.com>
2019-10-29 20:18:31 -0700

    Commiting updates

--- students/bishal_gupta/lesson03/slicing_lab.py
+++ students/bishal_gupta/lesson03/slicing_lab.py
@@ -5,24 +5,20 @@ Created on Tue Oct 29 15:38:52 2019
 @author: Bishal.Gupta
 """
 
-new_seq = ''
-seq = ''
+
 
 def exchange_first_last(seq):
     if type(seq) == tuple:
         new_seq = seq[::-1]
+        return new_seq
     elif type(seq) == list:
-        seq.reverse()
-        print(new_seq)
+        new_seq = seq.reverse()
+        return new_seq
     else:
-        print('nothing')
-        
-        
-exchange_first_last(seq)
-
-if __name__ == "__main__":
-    a_tuple = (2, 54, 13, 12, 5, 32)
-    assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+        return seq
+  
+res = exchange_first_last(seq)
+print(res)
     
     
         
