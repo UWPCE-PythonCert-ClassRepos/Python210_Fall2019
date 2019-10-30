5eaf364ed51c1e5c2daf9e28cc935e1d31fe7b9e
Jeff Scott <jscott@tableau.com>
2019-10-29 17:46:35 -0700

    Series-2 of list_lab

--- students/jeff_scott/lesson03/list_lab.py
+++ students/jeff_scott/lesson03/list_lab.py
@@ -42,7 +42,43 @@ for fruits in fruit:
         print(fruits)
 
 
+# Series-2
+
+"""Display the list from Series-1."""
+
+print(fruit)
+
+"""Remove the last fruit from the list."""
+
+fruit.pop()
+
+"""Display the list."""
+
+print(fruit)
+
+"""Ask the user for a fruit to delete, find it and delete it."""
+
+fruit = input("Which fruit would you like to delete? \
+Input the name of the fruit: ")
+if fruits in fruit:
+    fruit.remove(fruits)
+    print(fruit), "has been deleted"
+
+# Series-3
+
+"""Ask the user for input displaying a line like “Do you like apples?” for each fruit in the list 
+(making the fruit all lowercase)."""
 
 
+"""For each “no”, delete that fruit from the list."""
 
+"""For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values 
+(a while loop is good here)"""
 
+"""Display the list."""
+
+# Series-4
+
+"""Make a new list with the contents of the original, but with all the letters in each item reversed."""
+
+"""Delete the last item of the original list. Display the original list and the copy."""
