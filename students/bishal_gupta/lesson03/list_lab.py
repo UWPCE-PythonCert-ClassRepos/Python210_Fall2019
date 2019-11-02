2752d7b98d62a164227005f67437e95aa28ed9df
Bishal K Gupta <bishalg@gmail.com>
2019-11-02 14:15:16 -0700

    Lesson 03 Excercise solutions

--- students/bishal_gupta/lesson03/list_lab.py
+++ students/bishal_gupta/lesson03/list_lab.py
@@ -4,8 +4,8 @@ Created on Mon Oct 28 18:25:04 2019
 
 @author: Bishal.Gupta
 """
-response = input("a prompt for the user > ")
 
+#Series 1
 fruitlist = ['Apples','Pears','Oranges','Peaches']
 print("The list has following fruit", len(fruitlist), "fruits:", fruitlist)
 
@@ -35,6 +35,7 @@ for f in fruitlist:
     if 'P' in f:
         print(f)
         
+#Series 2        
 print("The list has following fruit", len(fruitlist), "fruits:", fruitlist)
 
 fruitlist.pop(-1)
@@ -46,6 +47,33 @@ delete_fruit = input("What fruit would you like to delete? ")
 fruitlist.remove(delete_fruit)
 
 print("The list has following fruit", len(fruitlist), "fruits:", fruitlist)
+
+#Series3
+fruitlist = ['Apples','Pears','Oranges','Peaches']
+print("The list has following fruit", len(fruitlist), "fruits:", fruitlist)
+
+lowercase_fruit = input("Do you like apples? ")
+fruit1 = fruitlist[0]
+fruit1 = fruit1.lower()
+fruitlist = [fruit1,'Pears','Oranges','Peaches']
+
+lowercase_fruit = input("Do you like Pears? ")
+fruit2 = fruitlist[1]
+fruit2 = fruit2.lower()
+fruitlist = [fruit1,fruit2,'Oranges','Peaches']
+
+lowercase_fruit = input("Do you like Oranges? ")
+fruit3 = fruitlist[3]
+fruit3 = fruit2.lower()
+fruitlist = [fruit1,fruit2,fruit3,'Peaches']
+
+lowercase_fruit = input("Do you like Peaches? ")
+fruit4 = fruitlist[0]
+fruit4 = fruit4.lower()
+fruitlist = [fruit1,fruit2,fruit3,fruit4]
+
+print("The list has following fruit", len(fruitlist), "fruits:", fruitlist)
+
 
         
         
--- students/bishal_gupta/lesson03/slicing_lab.py
+++ students/bishal_gupta/lesson03/slicing_lab.py
@@ -6,19 +6,31 @@ Created on Tue Oct 29 15:38:52 2019
 """
 
 
-
+#with the first and last items exchanged.
 def exchange_first_last(seq):
-    if type(seq) == tuple:
-        new_seq = seq[::-1]
-        return new_seq
-    elif type(seq) == list:
-        new_seq = seq.reverse()
-        return new_seq
-    else:
-        return seq
-  
-res = exchange_first_last(seq)
-print(res)
-    
+    return seq[-1:] + seq[1:-1] + seq[0:1]
+
+#with every other item removed.
+def remove_every_other_word(seq):
+    return seq[::2]
+
+#with the first 4 and the last 4 items removed, and then every other item in the remaining sequence
+def remove_first4_last4_and_every_other_item(seq):
+    return seq[4:-4][::2]
+
+#with the elements reversed (just with slicing).
+def reverse_elements(seq):
+    return seq[::-1]
+
+#with the last third, then first third, then the middle third in the new order.
+def last3_first3_middle3_Neworder(seq):
+    return seq[-3:] + seq[:3] + seq[3:-3]
+
+if __name__ ==  "__main__":
+    assert exchange_first_last('John Smith') == 'hohn SmitJ'
+    assert remove_every_other_word('John Smith') == 'Jh mt'
+    assert remove_first4_last4_and_every_other_item('First4Last4') == 'tL'
+    assert reverse_elements('John Smith') == 'htimS nhoJ'
+    assert last3_first3_middle3_Neworder('Last3First3MiddleOrder') == 'derLast3First3MiddleOr'
     
         
