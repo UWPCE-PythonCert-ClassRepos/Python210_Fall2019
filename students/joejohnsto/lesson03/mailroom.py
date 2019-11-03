3765933b68b4d1425861d7ac2ebf3615cb967c45
joejohnsto <joejohnsto@gmail.com>
2019-11-02 17:45:47 -0700

    Mailroom1 complete. lesson04 exercises added

--- students/joejohnsto/lesson03/mailroom.py
+++ students/joejohnsto/lesson03/mailroom.py
@@ -15,7 +15,7 @@ donor_db = [('Bill Gates', [4000.32, 35.00, 17899.99]),
             ('Donald Trump', [0.32, 5.00]),
             ('Denise Richards', [6040.77])]
 
-prompt = "\n".join(("Welcome to the mailroom!",
+prompt = "\n".join(("\nWelcome to the mailroom!",
           "Please choose from below options:",
           "1 - Send a Thank You",
           "2 - Create a Report",
@@ -29,42 +29,46 @@ def sort_key(donor):
 
 def thank_you():
     """Add a new donation to the database and print a thank you email for the donor"""
-    #add some way to let user quit back to main menu at any time (maybe call main? - if response = quit: main())
+    
     response = 'list'
     # get donor name from user, display donor list if asked
     while response == 'list':
-        response = input('What is the full name of the donor to whom you would like to\
-                     send a thank you?\nType "list" for a list of current donors. \
-                     or "q" to quit back to main menu.\n')
+        response = input('\nWhat is the full name of the donor to whom you would like to ' + \
+                     'send a thank you?\nType "list" for a list of current donors ' + \
+                     'or "q" to quit back to main menu.\n>>> ')
         if response == 'list':
             for x, y in donor_db:
                 print(x)
         elif response == 'q':
             main()
         else:
-            check = input(f'Is "{response}" the correct donor name?\n \
-                          Please respond "y" or "n"')
-            if check != 'y':
-                response = 'list'
-    # add donor to database if not already there   
+            check = input(f'Is "{response.title()}" the correct donor name?\n' + \
+                          'Please respond "y", "n", or "q" to quit back to main menu: ')
+            if check == 'q':
+                main()
+            elif check != 'y':
+                response = 'list'  
     name = response.title()
+    # get donation amount
+    amount = input(f'Enter the amount that {name} donated or type "q" to quit back to the main menu: ')
+    if amount == 'q':
+        main()
+    # add donor and new donation to database under donor name
     if not [x for x, y in donor_db if x == name]:
         donor_db.append((name, []))
-    # get donation amount
-    amount = float(input(f'Enter the amount that {name} donated: '))
-    # add new donation to database under donor name
     idx = [x for x, y in donor_db].index(name)
-    donor_db[idx][1].append(amount)
+    donor_db[idx][1].append(float(amount))
     #format an email thank you and print to the terminal
     ty_note = 'Dear Mr/Mrs {},\n\n' + \
                 'Thank you so much for your donation of ${:.2f}!\n' + \
                 'We here at RLC (Random Local Charity) really appreciate it!\n\n' + \
-                'Sincerely,\n\nBob Saget, CEO of RLC'
-    print(ty_note.format(name, amount))
+                'Sincerely,\n\nBob Saget, CEO of RLC\n'
+    print(ty_note.format(name, float(amount)))
 
 
 def create_report():
     """Print donor database, sorted by total donation amount"""
+    
     sorted_db = sorted(donor_db, key=sort_key, reverse=True)
     # build header line for report
     header = 'Donor Name          | Total Given | Num Gifts | Average Gift'
@@ -82,6 +86,8 @@ def create_report():
     
     
 def main():
+    """Prompt user to choose next action"""
+    
     while True:
         response = input(prompt)  # continuously collect user selection
         # now redirect to feature functions based on the user selection
