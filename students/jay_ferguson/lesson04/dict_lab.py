a9e4c6efd8285b12cda31d8cba01d947c4b73ed7
Jay Ferguson <ferguson.368@gmail.com>
2019-11-04 10:01:53 -0800

    Added set lab 01 function.

--- students/jay_ferguson/lesson04/dict_lab.py
+++ students/jay_ferguson/lesson04/dict_lab.py
@@ -14,6 +14,7 @@ def dict_lab_01():
     Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
     Display whether or not “Mango” is a value in the dictionary (i.e. True).
     """
+    print("\nDictionary Lab 01")
     chris_dict = {
         'Name': 'Chris',
         'City': 'Seattle',
@@ -47,6 +48,8 @@ def dict_lab_02():
     }
     """
 
+    print("\nDictionary Lab 02")
+
     chris_dict = {
         'Name': 'Chris',
         'City': 'Seattle',
@@ -61,8 +64,39 @@ def dict_lab_02():
     print(chris_dict_counted)
 
 
-try:
-    while True:
-        print("Hello!")
-except KeyboardInterrupt:
-    print("Phew! We're done.")
+def set_lab_01():
+    """
+
+    Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible by 2, 3 and 4 (figure out a way to compute those – don’t just type them in).
+    Display the sets.
+    Display if s3 is a subset of s2 (False)
+    and if s4 is a subset of s2 (True).
+    """
+
+    print("\nSet Lab 01")
+
+    def create_set(n):
+        """
+        Return a set of multiples of n that are in range 1-20 inclusive.
+        """
+        multiples = []
+        for i in range(1, 21):
+            if i % n == 0:
+                multiples.append(i)
+        return set(multiples)
+
+    s2 = create_set(2)
+    s3 = create_set(3)
+    s4 = create_set(4)
+
+    print('s2: ', s2)
+    print('s3:', s3)
+    print('s4', s4)
+    print('Is s3 a subset of s2? ', s3.issubset(s2))
+    print('Is s4 a subset of s2? ', s4.issubset(s2))
+
+
+if __name__ == '__main__':
+    dict_lab_01()
+    dict_lab_02()
+    set_lab_01()
