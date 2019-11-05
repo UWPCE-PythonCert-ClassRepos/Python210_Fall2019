44b1383cf0a61fa1eb2db5dad5dcf0e32493860b
buckcorter <bc.lastname@gmail.com>
2019-11-05 09:59:28 -0800

    Completed pseudo code for mailroom, modified function structure and added more functions to be called. Modified some dictionary keys to be more personal.

--- students/ben_carter/Mailroom/Mailroom.py
+++ students/ben_carter/Mailroom/Mailroom.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  4 18:54:44 2019
+Created on Tue Nov  5 08:47:12 2019
 
 @author: bclas
 """
+
 
 import sys
 
@@ -12,11 +13,19 @@ This code is for the Mailroom assignment, The code should be include a list of
 donors and how much they have donated. It should prompt the user to chose from 
 three options 1) send a thank you email, 2) Generate a report, 3)Exit the program
 """
-#The inital donor data structure 
+
+"""
+1) Build a dictionary with multiple donors to pull from
+2) prompt the user to select 1, 2, or 3
+3) in a function construct while loop
+"""
+
+
+
 donors = {
         "Bernie Sanders": [65234.82, 143.25],
-        "Bill Gates": [72134.41]
-        "Paul Allen": [540, 9000, 344.23, 231.15, 877]
+        "Freddie Mercury": [72134.41]
+        "Bob Dylan": [540, 9000, 344.23, 231.15, 877]
         "Winston Churchhill":[1874, 1965]
         "Carl Segan": [3245.42, 1996, 1934]
         }
@@ -29,11 +38,22 @@ prompt = "\n".join(("\nMailroom Script, Welcome!",
                     "3 ) Exit the script",
                     " "))
 
-def email():
-    """This function generates a thank you email with two arguments"""
+def mailroom():
+    """This function will contain the meat of the script which will inform the
+    script what function to call based on the users input at the first prompt"""
+
+def build_email():
+    """This function generates a thank you email """
+    
+def Send_thank_you():
+    """This function is called when the user inputs option 1. its than askes
+    the user for a donors name or takes the list input to display the names
+    of all the donors. it does with with a while loop until
+    user inputs q to exit"""
     
 def report():
-    """This function generates a formated report of all donors and some information regarding their donations"""
+    """This function generates a formated report of all donors and some
+    information regarding their donations"""
     
 def exit_program():
     """This function closes the program"""
