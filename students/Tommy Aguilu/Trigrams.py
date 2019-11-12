9a530d330a375837a5a523871f0cd034008b14df
pizzabagels <taguilu01@gmail.com>
2019-11-11 17:30:09 -0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	students/Tommy Aguilu/list_lab_01.py
    #	students/Tommy Aguilu/list_lab_02.py
    #	students/Tommy Aguilu/list_lab_03.py
    #	students/Tommy Aguilu/list_lab_04.py

--- students/Tommy Aguilu/Trigrams.py
+++ students/Tommy Aguilu/Trigrams.py
@@ -4,6 +4,8 @@
 import sys, random
 
 ###opens file from input
+import random
+
 def open_source():
     choice = input("please specify the file you would like to read")
     with open(choice, "r") as f:
@@ -17,19 +19,32 @@ def output_split(f):
 def build_trigrams(words):
     trigrams = {}
     for i in range(len(words)-2):
-        pair = words[i:i+2]
-        pair = tuple(pair)
+        pair = tuple(words[i:i+2])
         follow = words[i+2]
-        print(follow)
-        if pair in trigrams:
-            trigrams[pair] += [follow]
+
+        #scrub output
+
+        clean_pair = ("".join([i for i in pair[0] if i.isalpha()]), "".join([i for i in pair[1] if i.isalpha()]))
+        clean_follow = "".join([i for i in follow if i.isalpha()])
+
+        if clean_pair in trigrams:
+            trigrams[clean_pair] += [clean_follow]
         else:
-            trigrams[pair] = [follow]
+            trigrams[clean_pair] = [clean_follow]
     return trigrams
 
+def build_new(text_words):
+    output_list = []
+    for k in text_words:
+        random_key = random.randint(0,1)
+        random_value = random.randint(0,1)
+        output_list.append(k[random_key])
+        output_list.append(k[random_value])
+
+    for i in range(len(output_list)):
+        x = (" ".join(output_list))
+    return x
 
 
 if __name__ == "__main__":
-    words = (open_source().split())
-    print(words)
-    print(build_trigrams(words))
+    (build_new(build_trigrams((open_source().split()))))
