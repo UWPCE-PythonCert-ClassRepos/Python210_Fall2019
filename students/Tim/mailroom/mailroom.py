eed2d4996869f3b0f6535c73893d45720db19e95
Tim Dietz <tdietz05@uw.edu>
2019-11-19 12:23:08 -0800

    totalValue = 0# Please enter the commit message for your changes. Lines starting

--- students/Tim/mailroom/mailroom.py
+++ students/Tim/mailroom/mailroom.py
@@ -4,31 +4,45 @@ Created on Wed Nov  6 08:45:27 2019
 
 @author: tdietz
 """
+from datetime import date
 import sys
-donorList = [("Elon Musk", [45643.05, 60029090.04]), 
-             ("Hank Hill",[23.00, 457.47]), 
-             ("Wade Watts", [430594.56, 420.69]),
-             ("Stan Lee", [345698.45, 3490938.56]),
-             ("Col Mustard", [4345.45, 3434.46]),
-             ("Mark Normand", [45.00]),
-            ]
+donorList = {"Elon Musk": [45643.05, 60029090.04], 
+             "Hank Hill":[23.00, 457.47], 
+             "Wade Watts": [430594.56, 420.69],
+             "Stan Lee": [345698.45, 3490938.56],
+             "Col Mustard": [4345.45, 3434.46],
+             "Mark Normand": [45.00],
+            }
 
 def createReport():
-    print("Create Report Option")
-    #TODO create a function that generates a report
-    
+    print("This is the current report of", date.today(), ": ")
+    print("{:<30}|{:^15}|{:^12}|{:^14}".format('Donors Name', 'Total Given', ' # of Gifts', 'Average Gift'))
+    print('-'*73)    
+    for donor,values in donorList.items():
+        totalValue = 0
+        for values in donorList[donor]:
+            totalValue += values
+        avgValue = totalValue/len(donorList[donor])
+        numGifts = len(donorList[donor])
+        print('{:<30} ${:>14.2f}{:^12} ${:>13.2f}'.format(donor, totalValue, numGifts, avgValue))
     quitToMain()
 
 def sendThankYou():
-    nameOfPerson = input("Please enter a name (First Last): ")
+    #checks if the name is already in the current dictionary, second is if user types in 'list', third is adding a new entry to the dictionary
+    nameOfPerson = input("Please enter a name (First, Last) or type \'list\' to see the current list: ")
     if nameOfPerson in donorList:
-        print("TODO")
+        donationAmount = float(input("How much did %s donate?: " % nameOfPerson))
+        donorList[nameOfPerson].append(donationAmount)
+        print("{:s}, \n\n\tWe greatly appreciate your donation amount of ${:.2f}. \n\n Best, \n The Human Fund \n".format(nameOfPerson, donationAmount))
     elif nameOfPerson.lower() == 'list':
+        print("Donors currently in list: ")
         for key in donorList:
             print(key)
-            sendThankYou() #repromt the please enter a name
+        sendThankYou() #repromt the please enter a name
     else:
-        donorList.append(nameOfPerson,[])
+        donationAmount = float(input("How much did %s donate?: " % nameOfPerson))
+        print("{:s}, \n\n\tWe greatly appreciate your donation amount of ${:.2f}. \n\n Best, \n The Human Fund \n".format(nameOfPerson, donationAmount))
+        donorList[nameOfPerson] = [donationAmount]
         
     
     
