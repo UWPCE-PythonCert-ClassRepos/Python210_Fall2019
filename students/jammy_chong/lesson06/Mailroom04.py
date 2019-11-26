f35d06e3b1b0703798198a36b0261c3a4a0376ee
Jammy Chong <jammy087@gmail.com>
2019-11-25 23:11:40 -0800

    Completed Mailroom04 Unit Testing

--- students/jammy_chong/lesson06/Mailroom04.py
+++ students/jammy_chong/lesson06/Mailroom04.py
@@ -35,10 +35,17 @@ def create_letter(name):
     letter = (f"Dear {name},\n\n\tThank you for your very kind donation of " +
               f"${last_amount:.2f}.\n\n\tYou have donated {number_donations}" +
               f" times with an average of ${(average):.2f}" +
-              f" per donation.\n\n\tThe total amount you donated is " +
+              f" per donation.\n\n\tThe total amount you donated is $" +
               f"{total_given:.2f}.\n\n\tIt will be put to very good use.\n\n" +
               f"\t\t\tSincerely,\n\t\t\t\t-The Team.")
     return letter
+
+def display_donor_list():
+    display_string = "-"*30 + "\n"
+    for name in donor_list:
+        display_string += name + "\n"
+    display_string += ("-"*30)
+    return display_string
 
 # Menu Actions:
 
@@ -46,19 +53,15 @@ def create_letter(name):
 def send_thank_you():
     donor_input = ""
     donation_amount = ""
-    thank_you_prompt = "For main menu enter 'q' at any time.\n" + \
-                    "Enter a donor's Full Name or enter 'list' to see " + \
-                    "the donors list :"
+    thank_you_prompt = "For main menu enter 'q' at any time.\n" + "Enter a donor's Full Name or enter 'list' to see " + "the donors list :"
     dontation_prompt = "Please enter donation amount: "
+
     while donor_input != "q":
         donor_input = input(thank_you_prompt)
 
         # Donors list and exiting options
         while donor_input == "list":
-            print("-"*30)
-            for name in donor_list:
-                print(name)
-            print("-"*30)
+            print(display_donor_list())
             donor_input = input(thank_you_prompt)
         if donor_input == "q":
             break
@@ -70,43 +73,53 @@ def send_thank_you():
             try:
                 donation_amount = float(donation_amount)
             except ValueError:
-                print('Invalid donation amount')
+                print('Please enter a number')
 
-        # Assigning donor values
         if donation_amount == "q":
             break
+
+        # Assigning donor values
         if donor_input not in donor_list:
             add_donor(donor_input, [donation_amount])
         else:
             donor_list[donor_input].append(donation_amount)
-        print(donor_list[donor_input])
+
         print(create_letter(donor_input))
         donor_input = "q"
 
 
-def create_report():
+def display_report():
     # Using lambda to access values from list of dictionaries
     sorted_list = sorted(donor_list.items(),
                          key=lambda e: sum(e[1]), reverse=True)
-    print("Donor Name", " "*14, "| Total Given | Num Gifts | Average Gift")
-    print("-"*66)
+    report = "Donor Name" + " "*16 + "| Total Given | Num Gifts | Average Gift\n"
+    report += "-"*66 + "\n"
     for name in sorted_list:
-        print("{:25}  ${:>11.2f}{:>12d}  ${:>12.2f}".format(name[0],
-              sum(name[1]), len(name[1]), sum(name[1])/len(name[1])))
+        report += ("{:25}  ${:>11.2f}{:>12d}  ${:>12.2f}".format(name[0],
+              sum(name[1]), len(name[1]), sum(name[1])/len(name[1])) + "\n")
+    return report
+
+
+def create_report():
+    print(display_report())
+
+
+def create_text_files(path, date):
+    for name in donor_list:
+        string_letter = create_letter(name)
+        with open(path + name + '_' + date + '.txt', 'w') as f:
+            f.write(string_letter)
+            f.close()
 
 
 def send_thank_you_all():
     folder = input("Enter the folder to save the letters: ")
     path = pathlib.Path('./'+folder)
     if not path.is_dir(): os.mkdir("./"+folder)
-
+    folder_path = './' + folder_path +'./'
     date = str(datetime.now())[:-16]
 
-    for name in donor_list:
-        string_letter = create_letter(name)
-        with open('./' + folder + '/' + name + '_' + date + '.txt', 'w') as f:
-            f.write(string_letter)
-            f.close()
+    create_text_files(folder_path, date)
 
 
 def quit():
