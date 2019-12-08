1a70e5a1aac1805cd53b4a37b40cd665e1efd50f
joejohnsto <joejohnsto@gmail.com>
2019-12-07 19:44:18 -0800

    html renderer thru step5

---
+++ students/joejohnsto/lesson07/html_render.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+from io import StringIO
+
+
+# This is the framework for the base class
+class Element(object):
+    """
+    A class which is an element of html
+    Subclasses include common elements such as body, head, title, etc
+    
+    input
+    content: accepts strings or other Elements
+    kwargs: accepts html attributes as attribute="attribute definition"
+            or as a dictionary {attribute: attribute definition}
+    """
+
+    Tag = 'html'
+    def __init__(self, content=None, **kwargs):
+        self.content = []
+        self.attributes = kwargs
+        if content:
+            self.content.append(content)
+
+    @property
+    def open_tag(self):
+        if self.attributes:
+            att_list = [' ' + k + '="' + v + '"' for k, v in self.attributes.items()]
+            return self.Tag + ''.join(att_list)
+        else:
+            return self.Tag
+    
+    @property
+    def content_string(self):
+        content_string = ''
+        for _ in self.content:
+            try:
+                f = StringIO()
+                _.render(f)
+                content_string += f.getvalue()
+            except AttributeError:
+                content_string += _
+            finally:
+                if _ is not self.content[-1]:
+                    content_string += '\n'
+        return content_string
+    
+    def append(self, new_content):
+        self.content.append(new_content)
+
+    def render(self, out_file):
+        out_string = '<' + self.open_tag + '>\n' + self.content_string + '\n</' + self.Tag + '>'
+        out_file.write(out_string)
+
+
+class Html(Element):
+    Tag = 'html'
+
+
+class Body(Element):
+    Tag = 'body'
+
+
+class P(Element):
+    Tag = 'p'
+
+
+class Head(Element):
+    Tag = 'head'
+
+
+class onelinetag(Element):
+    
+    def render(self, out_file):
+        out_string = '<' + self.open_tag + '> ' + self.content_string + ' </' + self.Tag + '>'
+        out_file.write(out_string)
+
+
+class Title(onelinetag):
+    Tag = 'title'
+
+
+class SelfClosingTag(Element):
+    
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError('Content is not allowed in Self Closing Elements!')
+        super().__init__(content=None, **kwargs)
+    
+    def append(self):
+        raise TypeError('Content is not allowed in Self Closing Elements!')
+    
+    def render(self, out_file):
+        out_string = '<' + self.open_tag + ' />'
+        out_file.write(out_string)
+
+
+class Hr(SelfClosingTag):
+    Tag = 'hr'
+
+
+class Br(SelfClosingTag):
+    Tag = 'br'
